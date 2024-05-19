8008869b9156a1ccd0dd060957ec5246b78d2a96
r11i <wodeshouji12@gmail.com>
2024-05-19 18:21:44 +0700

    add feature to render notification logo accordingly on real time in the absence/presence of changes to the rental collection in firestore

--- src/component/topbar.js
+++ src/component/topbar.js
@@ -1,10 +1,32 @@
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, Alert } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
-import { Image } from 'expo-image'
+import { Image } from 'expo-image';
 import { Link } from 'expo-router';
+import { firestore } from '../config/firebase'; 
+import { collection, onSnapshot, query, where } from 'firebase/firestore';
 
 const Topbar = () => {
-    var username = 'Udin';
+    const [hasNotifications, setHasNotifications] = useState(false);
+    const username = 'Udin';
+
+    useEffect(() => {
+        const rentalCollectionRef = collection(firestore, 'Rental');
+    
+        const unsubscribe = onSnapshot(rentalCollectionRef, () => {
+            setHasNotifications(true); // Set notifications to true whenever changes occur
+        });
+    
+        return () => unsubscribe();
+    }, []);
+    
+
+    const handleNotificationClick = () => {
+        // Logic to navigate to the notification page and mark notifications as read
+        setHasNotifications(false);
+        Alert.alert('Navigating to notifications page...');
+        // Implement actual navigation logic here
+    };
 
     return (    
         <View style={styles.container}>
@@ -21,15 +43,21 @@ const Topbar = () => {
                 </View>
             </View>
             <View style={styles.right}>
-                <Link href='/'>
+                <Link href='/' onPress={handleNotificationClick}>
                     <View style={{flex:1, position:'absolute', alignItems:'center', justifyContent:'center'}}>
-                        <Image source={require('../../assets/functional/notification.svg')} contentFit='fill' style={{width:39, height:39}}/>
+                        <Image 
+                            source={hasNotifications 
+                                ? require('../../assets/nav/home-inact.svg') 
+                                : require('../../assets/functional/notification.svg')} 
+                            contentFit='fill' 
+                            style={{width:39, height:39}}
+                        />
                     </View>
                 </Link>
             </View>
         </View>
-    )
-}
+    );
+};
 
 const styles = StyleSheet.create({
     container: {
