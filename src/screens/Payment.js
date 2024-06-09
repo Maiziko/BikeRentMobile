61c9c95a6880820c9b80f8f1074d8c93455307cb
wildhanhidayatullah <18221024@std.stei.itb.ac.id>
2024-06-09 10:41:27 +0700

    update

--- src/screens/Payment.js
+++ src/screens/Payment.js
@@ -8,43 +8,43 @@ import Button from '../component/Button';
 import Topbar_2 from '../component/topbar_2';
 
 
-const VoucherItem = ({ title, description, image, onPress, isActive=false }) => (
-    <TouchableOpacity style={[styles.voucherItem, isActive && styles.activeVoucherItem]} onPress={onPress}>
-        <Image source={{ uri: image }} style={styles.voucherImage} />
-        <View style={styles.voucherTextContainer}>
-            <Text style={[styles.voucherTitle]}>{title}</Text>
-            <Text style={[styles.voucherDescription]}>{description}</Text>
+const CardItem = ({ title, account, image, onPress, isActive=false }) => (
+    <TouchableOpacity style={[styles.cardItem, isActive && styles.activeCardItem]} onPress={onPress}>
+        <Image source={{ uri: image }} style={styles.cardImage} />
+        <View style={styles.cardTextContainer}>
+            <Text style={[styles.cardTitle]}>{title}</Text>
+            <Text style={[styles.cardAccount]}>{account}</Text>
         </View>
     </TouchableOpacity>
 );
 
-const MyVoucher = () => {
-    const vouchers = [
+const Payment = () => {
+    const cards = [
         {
         id: '1',
-        title: 'Discount 20%',
-        description: 'Get 20% off on all items',
+        title: 'Mandiri',
+        account: '123456789',
         image: 'https://via.placeholder.com/150',
         },
         {
         id: '2',
         title: 'Free Shipping',
-        description: 'Free shipping on orders over $50',
+        account: '123456789',
         image: 'https://via.placeholder.com/150',
         },
         {
         id: '3',
         title: 'Buy 1 Get 1 Free',
-        description: 'Buy one get one free on selected items',
+        account: '123456789',
         image: 'https://via.placeholder.com/150',
         },
     ];
 
-    const [activeVoucherId, setActiveVoucherId] = useState(null);
-    const handleVoucherPress = (id) => {
-        setActiveVoucherId(id);
-        let desc = vouchers.find((voucher) => voucher.id === id).description;
-        Alert.alert('Voucher Berhasil Digunakan', desc);
+    const [activeCardId, setActiveCardId] = useState(null);
+    const handleCardPress = (id) => {
+        setActiveCardId(id);
+        let desc = cards.find((card) => card.id === id).account;
+        Alert.alert('Card Berhasil Digunakan', desc);
     };
 
     return (
@@ -59,23 +59,23 @@ const MyVoucher = () => {
                     </Link>
                 </View>
                 <View style={{width:'100%', alignItems:'center', justifyContent:'center'}}>
-                    <Text style={{color:'#FFFFFF', fontWeight:'bold', fontSize:21, textShadowColor: '#000', textShadowOffset: { width: 2, height: 2 }, textShadowRadius: 3,}}>My Voucher</Text>
+                    <Text style={{color:'#FFFFFF', fontWeight:'bold', fontSize:21, textShadowColor: '#000', textShadowOffset: { width: 2, height: 2 }, textShadowRadius: 3,}}>Pembayaran</Text>
                 </View>
             </View>
-            <Text style={{color:'#DC481A', fontWeight:'semibold', fontSize:20, marginVertical:10, marginLeft:15}}>Pilih Voucher</Text>
+            <Text style={{color:'#DC481A', fontWeight:'semibold', fontSize:20, marginVertical:10, marginLeft:15}}>Pilih Kartu Pembayaran</Text>
             <FlatList
-                data={vouchers}
+                data={cards}
                 renderItem={({ item }) => (
-                    <VoucherItem
+                    <CardItem
                         title={item.title}
-                        description={item.description}
+                        account={item.account}
                         image={item.image}
-                        isActive={item.id === activeVoucherId}
-                        onPress={() => handleVoucherPress(item.id)}
+                        isActive={item.id === activeCardId}
+                        onPress={() => handleCardPress(item.id)}
                     />
                 )}
                 keyExtractor={(item) => item.id}
-                contentContainerStyle={styles.voucherList}
+                contentContainerStyle={styles.cardList}
             />
             <View style={styles.bottom}>
                 <View style={{flexDirection:'row'}}>
@@ -90,8 +90,26 @@ const MyVoucher = () => {
                     <Text style={{color:'#5E5F60', fontWeight:'semibold', fontSize:16, left:5}}>Summary :</Text>
                     <Text style={{color:'#5E5F60', fontWeight:'bold', fontSize:16, position:'absolute', right:5}}>Rp85000</Text>
                 </View>
-                <Button children={'Konfirmasi'}/>
+                <Button children={'Bayar'}/>
             </View>
+            <Modal
+                transparent={true}
+                animationType="slide"
+                visible={modalVisible}
+                onRequestClose={handleClose}
+            >
+                <View style={{backgroundColor: 'rgba(0, 0, 0, 0.5)', padding: 10, width: '100%', height: '100%', marginLeft: 'auto', marginRight: 'auto'}}>
+                <View style={{backgroundColor: 'white', borderRadius: 20, marginTop: 'auto', marginBottom: 'auto'}}>
+                <View style={{padding: 20}}>
+                    <Image source={require('../../assets/GaLang.png')} style={{width: 133, height: 189, marginLeft: 'auto', marginRight: 'auto'}}></Image>
+                    <Image source={require('../../assets/TulisanGaLang.png')} style={{width: 117, height: 27, marginLeft: 'auto', marginRight: 'auto'}}></Image>
+                    <Text style={{color: '#459708', marginLeft: 'auto', marginRight: 'auto', marginTop: 20, fontWeight: 'bold', fontSize: 16}}>Pembayaran Berhasil</Text>
+                    <Text style={{color: '#004268', marginLeft: 'auto', marginRight: 'auto', marginTop: 10, fontWeight: 'bold', fontSize: 14}}>Terima kasih sudah menggunakan BikeRent</Text>
+                    <Pressable onPress={handleClose} style={{marginTop: 20, backgroundColor: '#459708', padding: 15, borderRadius: 10}}><Text style={{textAlign: 'center', color: 'white', fontWeight: 'bold'}}>OK</Text></Pressable>
+                </View>
+                </View>
+                </View>
+            </Modal>
         </View>
     )
 }
@@ -134,10 +152,10 @@ const styles = StyleSheet.create({
         shadowRadius: 100,
         elevation: 5,
     },
-    voucherList: {
+    cardList: {
         paddingHorizontal: 15,
     },
-    voucherItem: {
+    cardItem: {
         flexDirection: 'row',
         marginBottom: 10,
         padding: 10,
@@ -145,27 +163,27 @@ const styles = StyleSheet.create({
         backgroundColor: '#f9f9f9',
         elevation: 1,
     },
-    activeVoucherItem: {
+    activeCardItem: {
         backgroundColor: '#FFD4A8',
     },
-    voucherImage: {
+    cardImage: {
         width: 70,
         height: 70,
         borderRadius: 25,
         marginRight: 15,
     },
-    voucherTextContainer: {
+    cardTextContainer: {
         flex: 1,
         justifyContent: 'center',
     },
-    voucherTitle: {
+    cardTitle: {
         fontSize: 18,
         fontWeight: 'bold',
     },
-    voucherDescription: {
+    cardAccount: {
         fontSize: 14,
         color: '#666',
     },
 });
 
-export default MyVoucher;
+export default Payment;
